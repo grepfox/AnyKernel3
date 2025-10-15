23f8cd7f03e3d4b8836645809c0575af705fe534
grepfox <grepfox@tutamail.com>
2025-10-15 17:40:15 +0530

    Update anykernel.sh

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=PerfIgnite
+kernel.string="======[ Perf Kernel YAAP RC1 ]======"
 do.devicecheck=1
 do.modules=0
 do.systemless=1
